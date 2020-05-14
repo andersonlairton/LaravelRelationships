5e64a08a40e9035e4ef9806f8c2065b11e660a06
Anderson Lairton <lairton.anderson@gmail.com>
2020-05-14 14:05:54 -0300

    adicionando models e criando as migrations

---
+++ database/migrations/2020_05_14_164439_create_locations_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLocationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('locations', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('coutry_id')->unsigned(); //este campo sera o campo que fara o relacionamento com a tabela coutries
+            $table->foreign('coutry_id') //definindo que este campo é chave estrangeira
+                ->references('id') //referenciando campo da tabela que ele se relaciona
+                ->on('countries') //referenciando tabela
+                ->onDelete('cascade'); //e com o onDelete('cascade') delete os registros daquele pais caso ele seja deleteado,impedindo assim que gere registros orfaos
+            $table->integer('latitude');
+            $table->integer('longitude');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('locations');
+    }
+}
